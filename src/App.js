d9f3f8f44c027f5ffc5debabe2318acd417a8870
Geektimus <geektimus@users.noreply.github.com>
2017-03-16 12:08:04 -0500

    Changed sorting logic, Added actions to handle votes, moved sort to reducer.

--- src/App.js
+++ src/App.js
@@ -17,32 +17,6 @@ const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = { songs: [] };
-  }
-
-  compare(song_a, song_b) {
-    if (song_b.votes < song_a.votes) {
-      return -1;
-    }
-    if (song_b.votes > song_a.votes) {
-      return 1;
-    }
-    return 0;
-  }
-
-  songListShouldBeUpdated(songIndex) {
-    let songs = this.state.songs.map((song, index) => {
-      if (index === songIndex) {
-        console.log("adding vote to: ", song.name);
-        song.votes++;
-      }
-      return song;
-    }).sort(this.compare);
-    this.setState({ songs: songs });
-  }
-
   render() {
     return (
       <Provider store={store}>
@@ -52,7 +26,7 @@ class App extends Component {
             <Row>
               <Col lg={12} md={12} sm={12}>
                 <SongAdder />
-                <SongRanker handleListUpdates={this.songListShouldBeUpdated.bind(this)} songs={this.state.songs} />
+                <SongRanker />
               </Col>
             </Row>
           </Grid>
--- src/reducers/songs.js
+++ src/reducers/songs.js
@@ -2,17 +2,37 @@ const initialState = {
     songs: []
 }
 
+const compare = (song_a, song_b) => {
+    if (song_b.votes < song_a.votes) {
+        return -1;
+    }
+    if (song_b.votes > song_a.votes) {
+        return 1;
+    }
+    return 0;
+}
+
 const songs = (state = initialState, action) => {
+    let songs;
     switch (action.type) {
         case 'ADD_SONG':
             console.log('Adding song: ', action.song.name);
-            return Object.assign({}, state, { songs: [action.song, ...state.songs] });
+
+            songs = [action.song, ...state.songs];
+            songs = songs.sort(compare);
+
+            return Object.assign({}, state, { songs: songs });
         // This should be on a separated file for the vote's reducers.
         case 'ADD_VOTE':
-            let song = state.songs.filter(song => song.id === action.songId);
-            console.log("Adding vote to song: ", song[0].name);
+            let song = state.songs.filter(song => song.id === action.songId)[0];
+            console.log("Adding vote to song: ", song.name);
+            song.votes++;
 
-            return state;
+            songs = state.songs.filter(song => song.id !== action.songId);
+            songs.push(song);
+            songs.sort(compare);
+
+            return Object.assign({}, state, { songs: songs });
         default:
             return state;
     }
